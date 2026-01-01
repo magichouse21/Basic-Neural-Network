41aa35946e2e42d78f06a1781e3155a9a2059de1
magichouse21 <wizarding4life@gmail.com>
2025-12-31 17:21:58 -0800

    finished forward propogation

--- nn_v1.py
+++ nn_v1.py
@@ -1,14 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Much thanks to https://www.freecodecamp.org/news/building-a-neural-network-from-scratch/
 #our weights and biases are going to be in a vector (vectorized form)
 
-def init_params(layer_dims): #dimension of layers for initial parameters
-    np.random.seed(67) #random number gen but it's gonna be the same "random number" 67 cuz it funny rn
+def init_params(layer_dims): #dimension for all layers for initial parameters
+    np.random.seed(67) #random number gen but it's gonna be the same "random number" -- seed 67 cuz it funny rn --
     params = {} #dictionary 
     L = len(layer_dims)
 
     for l in range (1,L):
-        params["W"+str(l)] = np.random.randn(0,1)
+        params["W"+str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1])*0.01
+        params['B'+str(l)] = np.zeros(layer_dims[l])
 
+    return params
+
+def sigmoid(z):  #activation function
+    A = 1/(1+np.exp(-z))
+    cache = z
+
+    return A, cache
+
+#https://developers.google.com/machine-learning/crash-course/neural-networks/nodes-hidden-layers  ---this helps visualize it a bit---
+
+def forward_prop(X, params):  #inputs the weights and biases in parameters, X is first input layer aka training data
+    A = X
+    caches = []
+
+    for k in range (1, len(params)+1):
+        A_prev = A
+
+        Z = np.dot(params['W'+str(k)],A_prev) + params['B'+str(k)]
+        linear_cache = (A_prev, params['W'+str(k)], params['B'+str(k)])
+        A, activation_cache = sigmoid(Z)
+
+        cache = (linear_cache, activation_cache)
+        caches.append(cache)
     
+    return A, caches
+
+
+        
+
+
+
+
