3e27fa9aed0d0f4851e7c6ae8f4104cf3ab4f48b
magichouse21 <wizarding4life@gmail.com>
2026-01-08 23:25:15 -0800

    might have figured out forward prop

--- nn_self.py
+++ nn_self.py
@@ -8,7 +8,7 @@ import numpy as np
 # 123 + 234 = 5 
 
 #i think i might have it find the remainder of some added numbers where the divisor is 11
-#given inputs 123 and 234, add up the numbers so 357 with divisor 11, remainder is 5
+#input is three numbers 123, 234, 345, add up the numbers so 702 with divisor 11, remainder is 9
 #gotta start with smthn linear should take almost no iterations to be able to find the answer
 
 def initialize_network():
@@ -16,27 +16,26 @@ def initialize_network():
     np.random.seed(67)
 
     for k in range (0,2):   
-        layers['W'+str(k)] = np.random.randn(3,3) * 0.1
+        layers['W'+str(k)] = np.random.randn(3,3) * 0.1   #two layers both 3x3 matrices
         layers['B'+str(k)] = np.zeros(3)
 
     return layers
 
 def relu(x):
-    return np.max(0,x)
+    return np.maximum(0,x)
 
 def forward_propogation(input, layers): #given the input, how will we find the next layer stuff
-    new_layer = []
-    
-    for k in input:
-        z = layers['W'+str(k)]*input + layers['B'+str(k)]
-        a += z
-    a = relu(a)  #final output of the next neuron
-    new_layer.append(a)
+    prev_input = input  #we need this to progress through the hidden layers to foward prop until we hit the final output
+    print(len(layers))
+    for k in range (0, len(input)-1):
+        z = np.dot(layers['W'+str(k)], prev_input) + layers['B'+str(k)]
+        a = z 
+        prev_input = relu(a)  #final output of the next neuron and when the loop is over, we will have the output matrix
 
-
-    return new_layer
+    return prev_input
 
 def cost(output, actual):  #MSE cuz its super easy to understand
     return (output - actual)**2
 
-print(initialize_network())
+net = initialize_network()
+print(forward_propogation([123, 234, 345], net))
