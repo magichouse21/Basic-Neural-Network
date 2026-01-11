d6aaef8028764e761a1745faf825b0f25bbeb789
magichouse21 <wizarding4life@gmail.com>
2026-01-10 21:53:45 -0800

    tried to add scalability not working yet

--- nn_self.py
+++ nn_self.py
@@ -1,32 +1,34 @@
 import numpy as np
+
+#from keras.datasets import mnist
+#(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
 #im gonna try making it solely on my own, it's gonna go terribly
 
-# should look like this: 
+#i might as well use MNIST so i added it here in comments to save for later
 
-# input:
-
-# 123 + 234 = 5 
-
-#i think i might have it find the remainder of some added numbers where the divisor is 11
-#input is three numbers 123, 234, 345, add up the numbers so 702 with divisor 11, remainder is 9
-#gotta start with smthn linear should take almost no iterations to be able to find the answer
-
-def initialize_network():
+def initialize_network(input_dim):  #input dimensions
     layers = {}
+    L = input_dim
     np.random.seed(67)
 
-    for k in range (0,2):   
-        layers['W'+str(k)] = np.random.randn(3,3) * 0.1   #two layers both 3x3 matrices
-        layers['B'+str(k)] = np.zeros(3)
+    for k in range (0,L-1):   
+        layers['W'+str(k)] = np.random.randn(L,L) * 0.1   #two layers both 3x3 matrices
+        layers['B'+str(k)] = np.zeros(input_dim)
 
     return layers
 
-def relu(x):
+def relu(x): #activation function #1
     return np.maximum(0,x)
+
+def sigmoid(z):  #activation function  #2
+    return 1/(1+np.exp(-z))
+
 
 def forward_propogation(inputs, layers): #given the input, how will we find the next layer stuff
     prev_input = inputs  #we need this to progress through the hidden layers to foward prop until we hit the final output
     for k in range (0, len(inputs)-1):
+        print(layers['W'+str(k)], prev_input)
         z = np.dot(layers['W'+str(k)], prev_input) + layers['B'+str(k)]
         prev_input = relu(z)  #final output of the next neuron and when the loop is over, we will have the output matrix
 
@@ -35,6 +37,13 @@ def forward_propogation(inputs, layers): #given the input, how will we find the 
 def cost(output, actual):  #MSE cuz its super easy to understand
     return np.mean((output - actual)**2)
 
-net = initialize_network()
+def back_prop(inputs, layers):
+
+    return 0
+
+
+
+net = initialize_network(4)
 print(cost(forward_propogation([123, 234, 345], net), 9)) 
+
 
