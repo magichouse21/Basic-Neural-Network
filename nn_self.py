8bb6e53a2463836ba59ad0df608c945e5eb614b9
magichouse21 <wizarding4life@gmail.com>
2026-01-11 22:42:54 -0800

    fixed cost and forward prop works properly

--- nn_self.py
+++ nn_self.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def initialize_net(layer_dims): #variable is dimension of hidden layers
-    L = len(layer_dims)-1
+    L = len(layer_dims)
     layers = {}
     np.random.seed(67)
 
@@ -11,7 +11,24 @@ def initialize_net(layer_dims): #variable is dimension of hidden layers
     
     return layers
 
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
 
-net = initialize_net([1,2,3,2,1])
+def forward_prop(ipt, layers):
+    prev = ipt
+    L = len(layers)//2
 
-print(net)
+    for k in range (1, L+1): #except for the output which we want
+        z = np.dot(layers['W'+str(k)], prev) + layers['B'+str(k)]
+        prev = sigmoid(z)
+
+    return prev
+
+#https://medium.com/@anishnama20/understanding-cost-functions-in-machine-learning-types-and-applications-cd7d8cc4b47d  helped me decide
+def cost(ipt, actual): #categorical cross-entropy cuz imma try MNIST
+    n = len(ipt)
+    return -1/n * np.sum(np.sum(actual * np.log(ipt)))
+
+    
+net = initialize_net([3,5,3,2,4])
+print(cost(forward_prop([123, 234, 345], net), 5)) #find sum of 3 numbers, figure out remainder with divisor 17
